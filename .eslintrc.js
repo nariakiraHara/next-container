90d68a5261645ec98add21d0551e68ee464c0b88
nariakiraHara <nariakra@gmail.com>
2022-07-20 23:51:00 +0900

    feature:eslint, graphql

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'no-console': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+}
---
+++ pages/locations.tsx
@@ -0,0 +1,31 @@
+import { NextPage } from 'next'
+import { useQuery } from '@apollo/client'
+import { GET_LOCATIONS } from 'server/location'
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  return data.locations.map(({ id, name, description, photo }: any) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ))
+}
+
+export const Page: NextPage = () => {
+  return (
+    <>
+      <DisplayLocations />
+    </>
+  )
+}
+
+export default Page
